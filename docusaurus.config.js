23af1d94d8c4368d41ae10c06bbe011bb47359a8
周中良 <stackparagon@163.com>
2023-10-17 16:31:21 +0800

    feat: 配置修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'GitKenCom', // Usually your GitHub org/user name.
+  organizationName: 'UniverseExcellence', // Usually your GitHub org/user name.
   projectName: 'inkken', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
